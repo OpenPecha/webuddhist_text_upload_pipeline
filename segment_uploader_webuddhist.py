b81c79cea89e8596e05f35db1b6a637453fae75b
Tenzin  Tsering <tenzintsering630@gmail.com>
2025-09-30 09:34:27 +0530

    cleaned segment uploader script

--- segment_uploader_webuddhist.py
+++ segment_uploader_webuddhist.py
@@ -6,17 +6,8 @@ import unicodedata
 
 from utils import (
     get_token,
-    read_json_file,
-    fuzzy_match
+    read_json_file
 )
-
-WEBUDDHIST_API_URL = "https://api.webuddhist.com/api/v1/segments"
-
-TEXT_NAME = "heart_sutra"
-
-SEGMENT_PAYLOAD_FILE_PATH = f"{TEXT_NAME}/heart_sutra_payload/{TEXT_NAME}_root_text_segment_payload.json"
-
-SEGMENT_CONTENT_WITH_SEGMENT_ID_FILE_PATH = f"{TEXT_NAME}/heart_sutra_api_response/{TEXT_NAME}_segment_content_with_segment_id.json"
 
 LOG_FILE = "segment_upload_log.txt"
 
@@ -27,44 +18,46 @@ logging.basicConfig(
     format="%(asctime)s [%(levelname)s] %(message)s",
     level=logging.INFO,
 )
+
 logger = logging.getLogger(__name__)
 
-def upload_segments_to_webuddhist(data, token):
-    logger.info("Uploading segments to webuddhist")
-    response = requests.post(WEBUDDHIST_API_URL, json=data, headers={"Authorization": f"Bearer {token}"})
-    logger.info("Segments uploaded, ", response.status_code)
-    return response.json()
+class SegmentUploader:
+    def __init__(self, segment_upload_url: str = "https://api.webuddhist.com/api/v1/segments"):
+        self.text_name = input("Enter the text name: ")
+        self.data_file_path = f"{self.text_name}/{self.text_name}_root_text_segment_payload.json"
+        self.data = read_json_file(self.data_file_path)
+        self.segment_upload_url = segment_upload_url
+        self.segment_payload_file_path = f"{self.text_name}/heart_sutra_payload/{self.text_name}_root_text_segment_payload.json"
+        self.segment_content_with_segment_id_file_path = f"{self.text_name}/heart_sutra_api_response/{self.text_name}_segment_content_with_segment_id.json"
 
-def store_segment_content_with_segment_id_in_json(data):
-    logger.info("Storing segment content with segment id in json")
-    list_segment_content_with_segment_id = []
-    for _ in data["segments"]:
-        segment_content = _["content"]
-        list_segment_content_with_segment_id.append({"segment_content": segment_content, "id": _["id"]})
-    
-    with open(SEGMENT_CONTENT_WITH_SEGMENT_ID_FILE_PATH, "w", encoding="utf-8") as file:
-        json.dump(list_segment_content_with_segment_id, file, ensure_ascii=False, indent=4)
+    def upload_segments_to_webuddhist(self, token):
+        logger.info("Uploading segments to webuddhist")
+        response = requests.post(self.segment_upload_url, json=self.data, headers={"Authorization": f"Bearer {token}"})
+        logger.info("Segments uploaded, ", response.status_code)
+        return response.json()
+
+    def store_segment_content_with_segment_id_in_json(self, data):
+        logger.info("Storing segment content with segment id in json")
+        list_segment_content_with_segment_id = []
+        for _ in data["segments"]:
+            segment_content = _["content"]
+            list_segment_content_with_segment_id.append({"segment_content": segment_content, "id": _["id"]})
+        
+        with open(self.segment_content_with_segment_id_file_path, "w", encoding="utf-8") as file:
+            json.dump(list_segment_content_with_segment_id, file, ensure_ascii=False, indent=4)
 
 
-    logger.info("Segment content with segment id and hash id with segment content stored in json")
+        logger.info("Segment content with segment id and hash id with segment content stored in json")
 
+    def upload_segments(self):
+        token = get_token()
+        response = self.upload_segments_to_webuddhist(self.data, token)
+        self.store_segment_content_with_segment_id_in_json(response)
 
 
 if __name__ == "__main__":
-
-    logger.info("Starting upload process")
-
-    token = get_token()
-
-    logger.info("Token obtained")
-
-    logger.info("Reading file")
-
-    data = read_json_file(SEGMENT_PAYLOAD_FILE_PATH)
-
-    logger.info("File read")
     
-    response = upload_segments_to_webuddhist(data, token)
-
-    store_segment_content_with_segment_id_in_json(response)
+    segment_uploader = SegmentUploader()
+    
+    segment_uploader.upload_segments()
     
