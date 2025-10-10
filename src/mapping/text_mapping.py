fe1f2e9c3b2641b3139b932d3f71f1d995b556ce
Tenzin  Tsering <tenzintsering630@gmail.com>
2025-10-10 09:04:56 +0530

    Update heart sutra segment content and mapping files; remove obsolete commentary files
    - Added new segment content JSON files for heart sutra commentary 1 and root segments, updating segment IDs for consistency.
    - Removed outdated commentary 2 segment content and mapping files to streamline the data structure.
    - Updated the mapping data for heart sutra root and commentary 1, ensuring accurate references and IDs.
    - Adjusted the segment upload log to reflect recent uploads and storage actions for October 8, 2025.

--- src/mapping/text_mapping.py
+++ src/mapping/text_mapping.py
@@ -38,9 +38,14 @@ class CommentaryTextMapping:
     def __init__(self, mapping_upload_url: str = "https://api.webuddhist.com/api/v1/mappings"):
         self.mapping_upload_url = mapping_upload_url
 
+        self.root_text_id = input("Enter the root text id: ")
+        self.commentary_text_id = input("Enter the commentary text id: ")
+
         self.mapping_file_name = input("Enter the mapping file name: ")
         self.mapping_file_name_path = str(MAPPING_DATA_DIR / f"{self.mapping_file_name}.json")
         self.mapping_data = read_json_file(self.mapping_file_name_path)
+
+        self.commentary_number = input("Enter the commentary number: ")
         
         self.look_up_list_file_name_root = input("Enter the look up list file name for root: ")
         self.look_up_list_file_name_commentary = input("Enter the look up list file name for commentary: ")
@@ -53,77 +58,97 @@ class CommentaryTextMapping:
 
         self.mapping_payload_file_path = str(MAPPING_PAYLOAD_DIR / f"{self.mapping_file_name}_mapping_payload.json")
 
-    def validate_mapping_data_present_in_look_up_list(self):
-        last_found_root = 0
-        last_found_commentary = 0
+    def validate_mapping_root_segment_present_in_root_lookup_list(self):
+        last_found = 0
+
         for mapping in self.mapping_data:
-            root_found = False
-            commentary_found = False
-            if not mapping["root"] or len(mapping["root"]) == 0 or not mapping["commentary"] or len(mapping["commentary"]) == 0:
+            if not mapping["root_display_text"] or len(mapping["root_display_text"]) == 0:
                 continue
-            
-            for i in range(last_found_root, len(self.look_up_list_root)):
-                if fuzzy_match(self.look_up_list_root[i]["segment_content"], mapping["root"]):
-                    root_found = True
-                    last_found_root = i + 1
-                    break;
 
-            for i in range(last_found_commentary, len(self.look_up_list_commentary)):
-                if fuzzy_match(self.look_up_list_commentary[i]["segment_content"], mapping["commentary"]):
-                    commentary_found = True
-                    last_found_commentary = i + 1
-                    break;
+            root_text_found = False
+            for index in range(last_found, len(self.look_up_list_root)):
+                if fuzzy_match(self.look_up_list_root[index]["segment_content"], mapping["root_display_text"]):
+                    last_found = index + 1
+                    root_text_found = True
+                    break
 
-            if not root_found or not commentary_found:
-                raise ValueError(f"Mapping data {mapping} not found in look up list")
-            
+            if not root_text_found:
+                raise ValueError(f"Root {mapping['root_display_text']} not found in look up list\nMapping data: {mapping}")
+
         return True
-            
-    def replace_mapping_data_with_id(self):
-        for mapping in self.mapping_data:
-            
-            if not mapping["root"] or len(mapping["root"]) == 0:
+
+    def validate_commentary_lookup_list_present_in_mapping_data(self):
+        last_found = 0
+        for commentary_text in self.look_up_list_commentary:
+            if not commentary_text["segment_content"] or len(commentary_text["segment_content"]) == 0:
                 continue
 
-            for data in self.look_up_list_root:
-                if fuzzy_match(data["segment_content"], mapping["root"]):
-                    mapping["root"] = data["id"]
+            commentary_text_found = False
+            for index in range(last_found, len(self.mapping_data)):
+                if commentary_text["segment_content"] in self.mapping_data[index][f"commentary_{self.commentary_number}"]:
+                    commentary_text_found = True
+                    last_found = index + 1
+                    break;
+
+            if not commentary_text_found:
+                raise ValueError(f"Commentary {commentary_text['segment_content']} not found in mapping data\nMapping data: {self.mapping_data}")
+
+        return True
+                    
+            
+    def replace_mapping_root_display_text_with_id(self):
+        last_found = 0
+
+        for mapping in self.mapping_data:
+            if not mapping["root_display_text"] or len(mapping["root_display_text"]) == 0:
+                continue
+
+            root_text_found = False
+            for index in range(last_found, len(self.look_up_list_root)):
+                if fuzzy_match(self.look_up_list_root[index]["segment_content"], mapping["root_display_text"]):
+                    mapping["root_display_text"] = self.look_up_list_root[index]["id"]
+                    last_found = index + 1
+                    root_text_found = True
                     break
 
-            # Replace commentary text with ID
-            commentary_found = False
-            for data in self.look_up_list_commentary:
-                if fuzzy_match(data["segment_content"], mapping["commentary"]):
-                    mapping["commentary"] = data["id"]
-                    break
+            if not root_text_found:
+                raise ValueError(f"Root {mapping['root_display_text']} not found in look up list\nMapping data: {mapping}")
 
-        return self.mapping_data
+        return True
+
+    def get_mapping_dict(self):
+        mapping_dict = {}
+
+        for commentary_text in self.look_up_list_commentary:
+            if not commentary_text["segment_content"] or len(commentary_text["segment_content"]) == 0:
+                continue
+        
+            commentary_text_found = False
+            for index in range(0, len(self.mapping_data)):
+                if commentary_text["segment_content"] in self.mapping_data[index][f"commentary_{self.commentary_number}"]:
+                    if commentary_text["segment_content"] not in mapping_dict:
+                        mapping_dict[commentary_text["segment_content"]] = []
+                    mapping_dict[commentary_text["id"]].append(self.mapping_data[index]["root_display_text"])
+                    commentary_text_found = True
+                    break
+            
+            if not commentary_text_found:
+                raise ValueError(f"Commentary {commentary_text['segment_content']} not found in mapping data\nMapping data: {self.mapping_data}")
+
+        return mapping_dict
+
 
     def generate_mapping_payload(self):
+
+        mapping_dict: dict[str, List[str]] = self.get_mapping_dict()
+
         mapping_payload = []
 
-        for mapping in self.mapping_data:
-            if not mapping["root"] or len(mapping["root"]) == 0:
-                continue
+        for key, value in mapping_dict.items():
 
-            mapping_payload.append(
-                TextMapping(
-                    text_id=self.text_id,
-                    segment_id=mapping["commentary"],
-                    mappings=[
-                        ParentSegmentMapping(
-                            parent_text_id=mapping["root"],
-                            segments=[
-                                mapping["root"]
-                            ]
-                        )
-                    ]
-                )
-            )
 
-        return Mapping(
-            text_mappings=mapping_payload
-        )
+        
+                    
 
     def write_mapping_payload_to_file(self, mapping_payload):
         with open(self.mapping_payload_file_path, "w", encoding="utf-8") as file:
@@ -136,9 +161,10 @@ class CommentaryTextMapping:
 
 
     def map_text_and_upload_to_webuddhist(self):
-        self.validate_mapping_data_present_in_look_up_list()
+        self.validate_mapping_root_segment_present_in_root_lookup_list()
+        self.validate_commentary_lookup_list_present_in_mapping_data()
         
-        self.replace_mapping_data_with_id()
+        self.replace_mapping_root_display_text_with_id()
 
         mapping_payload = self.generate_mapping_payload()
 
